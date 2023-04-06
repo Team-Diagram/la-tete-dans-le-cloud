827e3daa05e2781fe1c5102ed88c5fc77ded23be
Doud75 <adrienquimbre@gmail.com>
2023-04-06 09:47:23 +0200

    adding script create user

---
+++ bash/create_user.sh
@@ -0,0 +1,24 @@
+# ssh heticusr@40.66.43.209 "bash -s" < ./script.sh "'test2' 'cle ssh'"
+
+RSA="$2"
+
+sudo useradd -m $1
+sudo chmod 755 /home/$1
+
+if [ ! -d /home/$1/.ssh ]
+then
+sudo mkdir /home/$1/.ssh
+fi
+
+sudo chmod 755 /home/$1/.ssh
+
+if [ ! -f /home/$1/.ssh/authorized_keys ]
+then
+sudo touch /home/$1/.ssh/authorized_keys
+fi
+
+sudo chmod 757 /home/$1/.ssh/authorized_keys
+
+echo $RSA >> /home/$1/.ssh/authorized_keys
+
+sudo chmod 754 /home/$1/.ssh/authorized_keys

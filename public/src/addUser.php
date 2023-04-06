d66592ddd1fcd0f727d0e4b9b4a59b8c992611ee
Doud75 <adrienquimbre@gmail.com>
2023-04-06 14:36:12 +0200

    truc

--- public/src/addUser.php
+++ public/src/addUser.php
@@ -8,8 +8,7 @@ if(!empty($_POST)){
         && !empty($_POST['name_user']) && !empty($_POST['key_user']) && !empty($_POST['server_user']) && !empty($_POST['bdd_user'])) {
             $nameUser = $_POST['name_user'];
             $keyUser = $_POST['key_user'];
-
-            $script1 = shell_exec("ssh heticusr@40.66.43.209 \"bash -s\" < ../bash/script.sh \"$nameUser $keyUser\"");
+            $script1 = shell_exec("ssh heticusr@40.66.43.209 \"bash -s\" < ../../bash/script.sh \"'$nameUser' '$keyUser'\"");
             echo "<pre>$script1</pre>";
     }
     //header("Location: homepage.php");

c5049ae3f2741d5eec1648774eec381610b1d54b
Nerilus <nerilus.h@gmail.com>
2023-04-07 10:37:34 +0200

    database

--- bash/create_db.sh
+++ bash/create_db.sh
@@ -1,39 +1,15 @@
-# ssh heticusr@40.66.43.209 "bash -s" < ./script.sh "'test2' 'cle ssh'"
+#!/bin/bash
+USER=$2
+DATABASE=$1
+PASSWORD=$3
 
-# Emplacement personnalisé pour les fichiers de base de données
-DATA_DIR="/opt/mysql-data"
 
-# Installation de Mysql
-# sudo apt update
-# sudo apt install mariadb-server
+if [ ! -d /home/$USER/ ]
+then
+  echo "user $USER doesn't exist"
+  exit 0
+fi
 
-# stopper le MySql
-sudo systemctl stop mariadb.service
-
-# Créer le répertoire de données
-sudo mkdir -p $DATA_DIR
-
-# Modifier les permissions pour le répertoire de données
-sudo chown -R mysql:mysql $DATA_DIR
-
-# Modifier l'emplacement des fichiers de base de données dans le fichier de configuration
-sudo sed -i "s|datadir=/var/lib/mysql|datadir=$DATA_DIR|" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# Démarrer la  MySQL
-sudo systemctl start mariadb.service
-
-# Connexion à MySQL
-sudo mysql -u root
-
-# Création de la base de données
-CREATE DATABASE groupe14;
-
-# Création de l'utilisateur
-CREATE USER 'fuegos'@'localhost' IDENTIFIED BY 'sosa78';
-
-# Attribution des droits à l'utilisateur
-GRANT ALL PRIVILEGES ON groupe14.*
-TO 'fuegos'@'localhost';
-
-# Quitter MySQL
-exit
+sudo mysql -e "CREATE DATABASE $DATABASE;"
+sudo myslq -e "CREATE USER '$USER'@'localhost' IDENTIFIED BY '$3';"
+sudo myslq -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USER'@'localhost';"

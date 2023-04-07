980a6fb3b30feb2ab709fe52b036e236522f6313
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-04-07 10:35:37 +0200

    fix : backup file variable user

--- bash/backup.sh
+++ bash/backup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-if [ ! -d /home/$USER/ ]
+
+username=$1
+
+if [ ! -d /home/$username/ ]
 then
   exit 0
 fi
 
 date=$(date '+%d-%m-%Y')
-username=$1
-
 tar cvzf /tmp/"$username"_"$date".tar --directory=/home/ "$username"

5f6ecc7e67f035a7bf4bcbf9ed867f0b108c7c35
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-04-25 16:00:58 +0200

    fix : script new password

--- bash/new_password.sh
+++ bash/new_password.sh
@@ -2,11 +2,11 @@
 USERNAME=$1
 PASSWORD=$2
 
-CHECK=`sudo cat /etc/passwd | grep $USER | cut -d ':' -f 1`
+CHECK=`sudo cat /etc/passwd | grep $USERNAME | cut -d ':' -f 1`
 
 if [ $USERNAME != $CHECK ]
 then
-  echo "user $USER doesn't exist"
+  echo "user $USERNAME doesn't exist"
   exit 0
 fi
 
--- public/src/newPassword.php
+++ public/src/newPassword.php
@@ -1,10 +1,11 @@
 <?php
 
+    
+
     if(!empty($_POST)){
         if(isset($_POST['username'],$_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])){
             $username = htmlspecialchars(strip_tags($_POST['username']));
             $password = htmlspecialchars(strip_tags($_POST['password']));
-
             $scriptNewPassword = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/new_password.sh $username $password");
             
             echo "<pre>$scriptNewPassword</pre>";

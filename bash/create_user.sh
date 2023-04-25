502da138b4904711910756c7c6d27b6474b88b63
Doud75 <adrienquimbre@gmail.com>
2023-04-25 13:51:04 +0200

    adding chmod authorized key

--- bash/create_user.sh
+++ bash/create_user.sh
@@ -4,4 +4,6 @@ USERNAME=$1
 
 sudo useradd -m $USERNAME
 
+sudo chmod 757 /home/$USERNAME/.ssh/authorized_keys
 echo $RSA >> /home/$USERNAME/.ssh/authorized_keys
+sudo chmod 754 /home/$USERNAME/.ssh/authorized_keys

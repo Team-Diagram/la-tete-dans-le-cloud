c2d04f7f4c566b7ac1a39619bd02e61a9c3a1a76
Doud75 <adrienquimbre@gmail.com>
2023-04-25 13:45:47 +0200

    changing create user

--- bash/create_user.sh
+++ bash/create_user.sh
@@ -3,22 +3,5 @@ RSA=$2
 USERNAME=$1
 
 sudo useradd -m $USERNAME
-sudo chmod 755 /home/$USERNAME
-
-if [ ! -d /home/$USERNAME/.ssh ]
-then
-sudo mkdir /home/$USERNAME/.ssh
-fi
-
-sudo chmod 755 /home/$USERNAME/.ssh
-
-if [ ! -f /home/$USERNAME/.ssh/authorized_keys ]
-then
-sudo touch /home/$USERNAME/.ssh/authorized_keys
-fi
-
-sudo chmod 757 /home/$USERNAME/.ssh/authorized_keys
 
 echo $RSA >> /home/$USERNAME/.ssh/authorized_keys
-
-sudo chmod 754 /home/$USERNAME/.ssh/authorized_keys

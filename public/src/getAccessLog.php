2c3f3126cab99160c41617e6589b4b25d138ff36
polinzz <paulinemiranda@hotmail.fr>
2023-04-27 15:15:13 +0200

    ip front

--- public/src/getAccessLog.php
+++ public/src/getAccessLog.php
@@ -3,11 +3,17 @@
 $uniqIp = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/uniqIp.sh");
 $dataLogByIp = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/dataLogByIp.sh");
 
-//var_dump($uniqIp);
-//var_dump($dataLogByIp);
+var_dump($uniqIp);
+var_dump($dataLogByIp);
+?>
 
-echo "<div class='users-storage'>";
-    for ($i = 0; $i < count($dataLogByIp); $i += 2) {
-    echo "<p class='user'>" . $dataLogByIp[$i] . "<span class='user-storage'>" . $dataLogByIp[$i+1] . "</span></p>";
-    }
-echo "</div>";
+<div class="consommation_stockage">
+    <div class="consommation_stockage_item">
+        <p class="size-18 font-medium">Stockage total</p>
+        <p class='space-total'><?= $uniqIp ?> kb</p>
+    </div>
+    <div class="consommation_stockage_item">
+        <p class="size-18 font-medium">Stockage utilisé</p>
+        <p class='space-used'><?= $dataLogByIp ?> kb</p>
+    </div>
+</div>

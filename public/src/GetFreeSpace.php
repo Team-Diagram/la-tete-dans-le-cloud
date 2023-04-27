6f51c99483f9e662bc228ae2d1aa966ae8325839
Doud75 <adrienquimbre@gmail.com>
2023-04-27 11:01:52 +0200

    no changing name

--- public/src/GetFreeSpace.php
+++ public/src/GetFreeSpace.php
@@ -2,8 +2,6 @@
 
 $totalSpace = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/getTotalSpace.sh");
 $availableSpace = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/getavailableSapce.sh");
-$usedSpace = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/getUsedSpace.sh");
 
 var_dump($totalSpace);
 var_dump($availableSpace);
-var_dump($usedSpace);
--- public/src/getFreeSpace.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$totalSpace = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/getTotalSpace.sh");
-$availableSpace = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/getavailableSapce.sh");
-
-var_dump($totalSpace);
-var_dump($availableSpace);

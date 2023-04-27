57f58611df7c65edde861903f565ac8f62dd78c3
Doud75 <adrienquimbre@gmail.com>
2023-04-27 11:26:14 +0200

    litll changes3

--- bash/failedPassword.sh
+++ bash/failedPassword.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo cat /var/log/auth.log | grep "Failed password" | grep '$1' | wc -l
+sudo cat /var/log/auth.log | grep "Failed password" | grep "$1" | wc -l
--- public/src/authLog.php
+++ public/src/authLog.php
@@ -4,6 +4,6 @@ $today = date("M j" );
 
 var_dump($today);
 
-$failedPassword = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/failedPassword.sh $today");
+$failedPassword = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/failedPassword.sh '$today'");
 
 var_dump($failedPassword);

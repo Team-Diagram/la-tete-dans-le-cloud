d2d6a4050575596b78dcf6f0e4411cbb7966d5af
polinzz <paulinemiranda@hotmail.fr>
2023-04-27 15:26:33 +0200

    test front user 2

--- public/src/getAccessLog.php
+++ public/src/getAccessLog.php
@@ -4,7 +4,7 @@ $uniqIp = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/uniqIp.sh");
 $dataLogByIp = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/dataLogByIp.sh");
 
 //var_dump($uniqIp);
-//var_dump($dataLogByIp);
+var_dump($dataLogByIp);
 
 for ($i = 0; $i < count($dataLogByIp); $i += 2) {
     echo "<p class='user'>" . $dataLogByIp[$i] . "<span class='user-storage'>" . $dataLogByIp[$i+1] . "</span></p>";

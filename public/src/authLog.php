2691e9c8ae7e01ac048668323fa484b727c303c0
Doud75 <adrienquimbre@gmail.com>
2023-04-27 11:38:03 +0200

    litll changes4

--- public/src/authLog.php
+++ public/src/authLog.php
@@ -2,8 +2,8 @@
 
 $today = date("M j" );
 
-var_dump($today);
-
 $failedPassword = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/failedPassword.sh '$today'");
+$sessionOpened = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/sessionOpened.sh '$today'");
 
 var_dump($failedPassword);
+var_dump($sessionOpened);

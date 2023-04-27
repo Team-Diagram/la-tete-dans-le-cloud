6503c4bddc88ae2c9591f3d5419d973a31eb8ffe
Doud75 <adrienquimbre@gmail.com>
2023-04-27 11:16:03 +0200

    get every failed password for today

---
+++ public/src/authLog.php
@@ -0,0 +1,9 @@
+<?php
+
+$today = date("M j" );
+
+var_dump($today);
+
+$failedPassword = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/failedPassword.sh $today");
+
+var_dump($failedPassword);

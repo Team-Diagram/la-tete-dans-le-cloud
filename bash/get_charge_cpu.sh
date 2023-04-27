a87d171ee852b985b75a583a769e53f83ceaea19
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-04-27 13:21:08 +0200

    feat : calcul cpu charge

--- bash/get_charge_cpu.sh
+++ bash/get_charge_cpu.sh
@@ -4,6 +4,10 @@ FIRST_LINE=$(grep '^cpu' /proc/stat)
 
 read cpu user nice system idle iowait irq softirq steal guest guest_nice <<< "$FIRST_LINE"
 
-cpu_time=$((user + nice + system + irq + softirq + steal + guest + guest_nice))
+cpu_time_all_processess=$((user + nice + system + irq + softirq + steal + guest + guest_nice))
 
-echo $cpu_time
+cpu_time_last_statement=$((user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice))
+
+cpu_accurate=("100*$cpu_time_all_processess/$cpu_time_last_statement")
+
+echo $cpu_accurate

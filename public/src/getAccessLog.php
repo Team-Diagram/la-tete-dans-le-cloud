cae24afb9d73bd0ac1a95411583b7ea044baad4a
polinzz <paulinemiranda@hotmail.fr>
2023-04-27 15:21:27 +0200

    ip fix 2

--- public/src/getAccessLog.php
+++ public/src/getAccessLog.php
@@ -5,15 +5,20 @@ $dataLogByIp = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/dataLog
 
 var_dump($uniqIp);
 var_dump($dataLogByIp);
+
+for ($i = 0; $i < count($dataLogByIp); $i += 2) {
+    echo "<p class='user'>" . $dataLogByIp[$i] . "<span class='user-storage'>" . $dataLogByIp[$i+1] . "</span></p>";
+}
 ?>
 
 <div class="consommation_stockage">
     <div class="consommation_stockage_item">
-        <p class="size-18 font-medium">Stockage total</p>
-        <p class='space-total'><?= $uniqIp ?> kb</p>
+        <p class="size-18 font-medium">Nombre total d'IP</p>
+        <p class='space-total'><?= $uniqIp ?></p>
     </div>
     <div class="consommation_stockage_item">
         <p class="size-18 font-medium">Stockage utilisé</p>
-        <p class='space-used'><?= $dataLogByIp ?> kb</p>
+<!--        <p class='space-used'>--><?//= $dataLogByIp ?><!--</p>-->
     </div>
+
 </div>

99e19d3b9411fb547df69def8efcf7bb259bcc35
polinzz <paulinemiranda@hotmail.fr>
2023-04-27 18:37:44 +0200

    test barre fix

--- public/src/getCPU.php
+++ public/src/getCPU.php
@@ -1,14 +1,14 @@
 <?php
-    $chargeCPU = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/get_charge_cpu.sh");
+    $chargeCPU = shell_exec("bash /var/www/html/la-tete-dans-le-cloud/bash/get_charge_cpu.sh") + "%";
     $chargeCPUin = str_replace(' ', '', $chargeCPU);
 ?>
 <div class="consommation_stockage">
     <div class="consommation_stockage_item">
         <p class="size-18 font-medium">Consommation</p>
-        <p class='space-total'><?= $chargeCPU ?> %</p>
+        <p class='space-total'><?= $chargeCPU ?>%</p>
     </div>
 </div>
 
 <div class='stockage-bar'>
-    <div class='stockage-bar-used' style="width: <?= $chargeCPUin ?>%;"></div>
+    <div class='stockage-bar-used' style="width: <?= $chargeCPU ?>;"></div>
 </div>

952b88d54f068698f18943ccf8d692935d24e645
Doud75 <adrienquimbre@gmail.com>
2023-04-27 16:00:14 +0200

    debug

--- public/src/getAccessLog.php
+++ public/src/getAccessLog.php
@@ -9,19 +9,19 @@ $dataLogByIp = array_values($dataLogByIp);
 //var_dump($uniqIp);
 var_dump($dataLogByIp);
 
-for ($i = 0; $i < count($dataLogByIp); $i += 2) {
+/*for ($i = 0; $i < count($dataLogByIp); $i += 2) {
     echo "<p class='user'>" . $dataLogByIp[$i] . "<span class='user-storage'>" . $dataLogByIp[$i+1] . "</span></p>";
 }
-?>
+*/?><!--
 
 <div class="consommation_stockage">
     <div class="consommation_stockage_item">
         <p class="size-18 font-medium">Nombre total d'IP</p>
-        <p class='space-total'><?= $uniqIp ?></p>
+        <p class='space-total'><?php /*= $uniqIp */?></p>
     </div>
     <div class="consommation_stockage_item">
         <p class="size-18 font-medium">Stockage utilisé</p>
-        <p class='space-used'><?= $dataLogByIp ?></p>
+        <p class='space-used'><?php /*= $dataLogByIp */?></p>
     </div>
 
-</div>
+</div>-->

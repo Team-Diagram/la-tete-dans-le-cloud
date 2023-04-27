26d34ebff795b32b505a99b5f96adbee9781c7fb
Doud75 <adrienquimbre@gmail.com>
2023-04-27 14:15:17 +0200

    get access_log log3

--- bash/dataLogByIp.sh
+++ bash/dataLogByIp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo cat /var/log/nginx/access.log | cut -d ' ' -f 1 | sort -u > /var/log/nginx/tmpFile
+sudo cat /var/log/nginx/access.log | cut -d ' ' -f 1 | sort -u |
 while read p
 do
-WC=`cat /var/log/nginx/access.log | grep $p | wc -l`
+WC=`sudo cat /var/log/nginx/access.log | grep $p | wc -l`
 GEO=`curl http://ipinfo.io/$p 2> /dev/null | jq '.. | .country' 2> /dev/null`
 echo "$p : $WC | $GEO"
-done < /var/log/nginx/tmpFile
-rm /var/log/nginx/tmpFile
+done

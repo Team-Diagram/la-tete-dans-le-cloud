b37cbb685b27c15c8672c5fea0f5cd4c43bf38d3
Romain Lhuillier <romainlhuillier94@gmail.com>
2023-04-27 13:40:55 +0200

    fix : new formated cpu

--- bash/get_charge_cpu.sh
+++ bash/get_charge_cpu.sh
@@ -8,6 +8,9 @@ cpu_time_all_processess=$((user + nice + system + irq + softirq + steal + guest 
 
 cpu_time_last_statement=$((user + nice + system + idle + iowait + irq + softirq + steal + guest + guest_nice))
 
-cpu_accurate=$(bc <<< "scale=2; 100 * $cpu_time / $total_time")
+cpu_accurate=$(echo "scale=2; 100 * $cpu_time_all_processess / $cpu_time_last_statement" | bc)
 
-echo "$cpu_accurate";
+cpu_formatted=$(printf "%.2f" $cpu_accurate)
+cpu_formatted=$(printf "%0.2f" $cpu_formatted)
+
+echo $cpu_formatted
